63d5c9e07f1227e68a851c4846134c09685118c2
Morgan Newman <mpnewman95@gmail.com>
2018-11-30 13:57:11 -0800

    Adds interactivity to stats page

--- src/components/Learn/Stats.js
+++ src/components/Learn/Stats.js
@@ -56,8 +56,14 @@ class Stats extends Component {
 		// Redirect if user not authenticated
 		if (!this.props.loggedIn) return <Redirect to="/" />;
 
-		// const { cardsAnswered, maxStreak, streak } = this.props.userStats;
-		// const { seen, unseen, learning, mastered } = this.processCardStats();
+		const { cardsAnswered, maxStreak, streak } = this.props.userStats;
+		const { seen, unseen, learning, mastered } = this.processCardStats();
+
+		const percentSeen = (seen / (seen + unseen)) * 100;
+		const percentUnseen = (unseen / (seen + unseen)) * 100;
+		const percentMastered = (mastered / (seen + unseen)) * 100;
+		const percentLearning = (learning / (seen + unseen)) * 100;
+
 		return (
 			<div className="stats__container">
 				<main className="stats">
@@ -65,32 +71,47 @@ class Stats extends Component {
 					<section className="stats-progress">
 						<h2 className="stats-subheading">Progress</h2>
 						<div className="stats-progress-bar">
-							<div className="stats-progress-bar__seen" />
-							<div className="stats-progress-bar__unseen" />
+							<div
+								className="stats-progress-bar__seen"
+								style={{ width: (percentSeen > 5 ? percentSeen : 5) + '%' }}
+							/>
+							<div
+								className="stats-progress-bar__unseen"
+								style={{ width: (percentUnseen > 5 ? percentUnseen : 5) + '%' }}
+							/>
 						</div>
 						<div className="stats-progress-bar-text">
-							<p>50% Seen</p>
-							<p>50% Unseen</p>
+							<p>{percentSeen}% Seen</p>
+							<p>{percentUnseen}% Unseen</p>
 						</div>
 					</section>
 					<section className="stats-mastery">
 						<h2 className="stats-subheading">Mastery</h2>
 						<div className="stats-mastery-chart">
 							<div className="stats-mastery-chart-bar__container">
-								<div className="stats-mastery-chart-bar stats-mastery-chart-bar__unseen">
-									<p className="stats-mastery-chart-bar__percentage">10%</p>
+								<div
+									style={{ width: (percentUnseen > 15 ? percentUnseen : 15) + '%' }}
+									className="stats-mastery-chart-bar stats-mastery-chart-bar__unseen"
+								>
+									<p className="stats-mastery-chart-bar__percentage">{percentUnseen}%</p>
 								</div>
 								<p className="stats-mastery-chart-text">New</p>
 							</div>
-							<div className="stats-mastery-chart-bar__container">
+							<div
+								style={{ width: (percentLearning > 15 ? percentLearning : 15) + '%' }}
+								className="stats-mastery-chart-bar__container"
+							>
 								<div className="stats-mastery-chart-bar stats-mastery-chart-bar__learning">
-									<p className="stats-mastery-chart-bar__percentage">40%</p>
+									<p className="stats-mastery-chart-bar__percentage">{percentLearning}%</p>
 								</div>
 								<p className="stats-mastery-chart-text">Learning</p>
 							</div>
 							<div className="stats-mastery-chart-bar__container">
-								<div className="stats-mastery-chart-bar stats-mastery-chart-bar__mastered">
-									<p className="stats-mastery-chart-bar__percentage">20%</p>
+								<div
+									className="stats-mastery-chart-bar stats-mastery-chart-bar__mastered"
+									style={{ width: (percentMastered > 15 ? percentMastered : 15) + '%' }}
+								>
+									<p className="stats-mastery-chart-bar__percentage">{percentMastered}%</p>
 								</div>
 								<p className="stats-mastery-chart-text">Mastered</p>
 							</div>
@@ -100,8 +121,20 @@ class Stats extends Component {
 						<h2 className="stats-subheading">Fun Facts</h2>
 						<div className="stats-fun-facts-fact">
 							<p>
-								<span className="stats-fun-facts-fact__title">Longest streak:</span>{' '}
-								<span className="stats-fun-facts-fact__num">15</span> cards correct
+								<span className="stats-fun-facts-fact__title">Total cards answered:</span>{' '}
+								<span className="stats-fun-facts-fact__num">{cardsAnswered}</span> cards
+							</p>
+						</div>
+						<div className="stats-fun-facts-fact">
+							<p>
+								<span className="stats-fun-facts-fact__title">Longest correct streak:</span>{' '}
+								<span className="stats-fun-facts-fact__num">{maxStreak}</span> cards correct
+							</p>
+						</div>
+						<div className="stats-fun-facts-fact">
+							<p>
+								<span className="stats-fun-facts-fact__title">Current correct streak:</span>{' '}
+								<span className="stats-fun-facts-fact__num">{streak}</span> cards correct
 							</p>
 						</div>
 					</section>
