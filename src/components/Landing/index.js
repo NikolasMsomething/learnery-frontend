ec7d4e9035ac4935eba7f9412616d49b36de68e2
Morgan Newman <mpnewman95@gmail.com>
2019-01-07 19:03:09 -0800

    Updates github icon to be SVG

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,66 +1,66 @@
 // Styles
-import "./Landing.scss";
-import "./Modals.scss";
+import './Landing.scss';
+import './Modals.scss';
 // Modules
-import React, { Component } from "react";
+import React, { Component } from 'react';
 // Redux
-import { connect } from "react-redux";
-import { openRegisterModal, openLoginModal } from "../../controller/actions";
+import { connect } from 'react-redux';
+import { openRegisterModal, openLoginModal } from '../../controller/actions';
 // Components
 // import Footer from "../Footer";
-import LoginModal from "./Login/LoginModal";
-import RegisterModal from "./Register/RegisterModal";
+import LoginModal from './Login/LoginModal';
+import RegisterModal from './Register/RegisterModal';
 // Assets
-const LearneryHero = require("../../assets/Learnery_Logo.png");
-const GithubIcon = require("../../assets/GitHub-Mark-64px.png");
+import { FaGithub as GithubIcon } from 'react-icons/fa';
+const LearneryHero = require('../../assets/Learnery_Logo.png');
 
 class Landing extends Component {
-	openRegisterModal = () => this.props.dispatch(openRegisterModal());
-	openLoginModal = () => this.props.dispatch(openLoginModal());
+  openRegisterModal = () => this.props.dispatch(openRegisterModal());
+  openLoginModal = () => this.props.dispatch(openLoginModal());
 
-	render() {
-		const { showLoginModal, showRegisterModal } = this.props;
-		return (
-			<main className="App">
-				<header className="headerBar">
-					<a
-						rel="noopener noreferrer"
-						target="_blank"
-						href="https://github.com/NikolasMsomething/learnery-frontend"
-					>
-						<img alt="" src={GithubIcon} className="learnArrow" />
-					</a>
-					{/* TODO: Add state to manage being logged in */}
-					<button className="yellow-button" onClick={this.openLoginModal}>
-						<p>Login</p>
-					</button>
-				</header>
-				{showLoginModal && <LoginModal />}
-				{showRegisterModal && <RegisterModal />}
-				<section className="registerSection">
-					<img className="LearneryHero" src={LearneryHero} alt="" />
-					<p className="registerFirstP">
-						Will you pass your software <br /> engineering interviews?
-					</p>
-					<p className="registerSecondP">
-						Get ahead of the pack by practicing programming trivia on Learnery!{" "}
-					</p>
-					{/* TODO: Add state to manage being logged in */}
+  render() {
+    const { showLoginModal, showRegisterModal } = this.props;
+    return (
+      <main className="App">
+        <header className="headerBar">
+          <a
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://github.com/NikolasMsomething/learnery-frontend"
+          >
+            <GithubIcon className="learnArrow" />
+          </a>
+          {/* TODO: Add state to manage being logged in */}
+          <button className="yellow-button" onClick={this.openLoginModal}>
+            <p>Login</p>
+          </button>
+        </header>
+        {showLoginModal && <LoginModal />}
+        {showRegisterModal && <RegisterModal />}
+        <section className="registerSection">
+          <img className="LearneryHero" src={LearneryHero} alt="" />
+          <p className="registerFirstP">
+            Will you pass your software <br /> engineering interviews?
+          </p>
+          <p className="registerSecondP">
+            Get ahead of the pack by practicing programming trivia on Learnery!{' '}
+          </p>
+          {/* TODO: Add state to manage being logged in */}
 
-					<button className="green-button" onClick={this.openRegisterModal}>
-						<p>Get Started!</p>
-					</button>
-				</section>
-				{/* <Footer /> */}
-			</main>
-		);
-	}
+          <button className="green-button" onClick={this.openRegisterModal}>
+            <p>Get Started!</p>
+          </button>
+        </section>
+        {/* <Footer /> */}
+      </main>
+    );
+  }
 }
 
-const mapStateToProps = state => ({
-	showLoginModal: state.landing.showLoginModal,
-	showRegisterModal: state.landing.showRegisterModal,
-	loggedIn: state.auth.loggedIn
+const mapStateToProps = (state) => ({
+  showLoginModal: state.landing.showLoginModal,
+  showRegisterModal: state.landing.showRegisterModal,
+  loggedIn: state.auth.loggedIn,
 });
 
 export default connect(mapStateToProps)(Landing);
